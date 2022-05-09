5f0590335f6470c424e1186ae67efbb3f2c28043
Tareq Hassan <tareqhassan2014@gmail.com>
2022-05-09 19:06:04 +0600

    update product controller

--- src/resources/product/product.controller.ts
+++ src/resources/product/product.controller.ts
@@ -2,7 +2,7 @@ import { deleteOne, getAll, getOne, updateOne } from '../../utility/factory.js';
 import { createOne } from './../../utility/factory.js';
 import ProductModel from './product.model.js';
 
-export const getAllProduct = getAll(ProductModel);
+export const getAllProduct = getAll(ProductModel, { path: 'userId' });
 export const createProduct = createOne(ProductModel);
 export const updateProduct = updateOne(ProductModel);
 export const getProductByID = getOne(ProductModel);
